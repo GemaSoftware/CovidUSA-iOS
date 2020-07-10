9bfd6002c33b1e3b97f1a3d745b5fe2c14e809cb
Agron Gemajli <agrongemajli@outlook.com>
2020-07-10 19:05:47 -0400

    Initial Commit

---
+++ Covid-19 Tool/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid-19 Tool
+//
+//  Created by Agron Gemajli on 7/10/20.
+//  Copyright © 2020 Agron Gemajli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
